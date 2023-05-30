480f9bbc1b8179563ca8988ecc15577b95060fb0
weipengyan <weipengyan1123@163.com>
2023-05-30 18:14:40 +0800

    点对点RPC调用测试完成

---
+++ build/src/CMakeFiles/mprpc.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "../../lib/libmprpc.a"
+  "../../lib/libmprpc.pdb"
+  "CMakeFiles/mprpc.dir/MprpcApplication.cpp.o"
+  "CMakeFiles/mprpc.dir/MprpcChannel.cpp.o"
+  "CMakeFiles/mprpc.dir/MprpcConfig.cpp.o"
+  "CMakeFiles/mprpc.dir/RpcProvider.cpp.o"
+  "CMakeFiles/mprpc.dir/rpcheader.pb.cc.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mprpc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
